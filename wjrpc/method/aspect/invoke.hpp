1d15d99a5f2259ad34af63ed63e26f4e3053f388
Vladimir Migashko <migashko@gmail.com>
2017-05-31 17:28:45 +0300

    tmpl upd

--- wjrpc/method/aspect/invoke.hpp
+++ wjrpc/method/aspect/invoke.hpp
@@ -75,9 +75,10 @@ struct invoke: Handler
       using namespace std::placeholders;
       auto ph = std::make_shared<incoming_holder>( std::move(holder) );
       Handler::operator()( t, std::move(req), 
-        [this, ph, outgoing_handler]( result_ptr result, error_ptr err )
+        [ph, outgoing_handler]( result_ptr result, error_ptr err )
         {
-          self::callback_<T, TT>(ph, outgoing_handler, std::move(result), std::move(err) );
+          ::wjrpc::invoke<JParams, JResult, Handler, JError>
+                 ::template callback_<T, TT>(ph, outgoing_handler, std::move(result), std::move(err) );
         }
       );
     }
