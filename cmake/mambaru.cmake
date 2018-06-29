c00b96175f39c1aa957d2c1c19ba55339a038ad1
Vladimir Migashko <migashko@gmail.com>
2018-06-29 18:55:58 +0300

    *cmake

--- cmake/mambaru.cmake
+++ cmake/mambaru.cmake
@@ -6,6 +6,16 @@ endif()
 include(mambaopt)
 include(mambalibs)
 
+get_faslib()
+get_mambaru(wjson WJSON_DIR "")
+
+if (NOT WJRPC_DISABLE_LOG)
+  get_mambaru(wlog WLOG_DIR WLOG_LIB)
+else()
+  add_definitions(-DWJRPC_DISABLE_LOG)
+endif()
+
+
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
 
