6467aebeb39a494828fb7005d6cffbe81bb34d4f
Vladimir Migashko <migashko@gmail.com>
2018-11-02 19:32:06 +0300

    cmake-ci upgrade autocommit

--- .ci/cppcheck.sh
+++ .ci/cppcheck.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-script_name=$(readlink -e $0)
-script_dir=$(dirname $script_name)
-prj_dir=$(dirname $script_dir)
-
-echo "$prj_dir"
-$script_dir/cppcheck-exec.sh $@ $prj_dir |& tee /tmp/cppcheck.cppcheck 
-sh -c "! grep '\[' /tmp/cppcheck.cppcheck"
-exit $?
+../external/cmake-ci/ci/cppcheck.sh
