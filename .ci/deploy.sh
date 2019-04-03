451877545039055f0897c9ba15663f764c9e3c91
gitlab-runner <migashko@gmail.com>
2019-04-03 03:19:13 +0300

    Night Build cmake-ci upgrade autocommit

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -10,12 +10,27 @@ else
   prj="$1"
 fi
 
+log=/logs/${prj}.log
+dname="${prj}d"
+
+backup="../${prj}-$(date +%F-%X)"
+sudo -u wwwrun mkdir $backup
+sudo -u wwwrun mv $PWD/* $backup
+
 dir="/tmp/$USER/.deploy/$prj"
 arc="replaceme"
 mkdir -m 0777 -p "$dir"
 
-scp "gitlab-runner@repobuild2:/monamour/$prj/$arc" "$dir/"
-tar -xf "$dir/$arc" -C "$dir"
+scp "gitlab-runner@repobuild2:/monamour/$prj/$arc" "$dir/" || exit 1
+tar -xf "$dir/$arc" -C "$dir" || exit 1
 rm "$dir/$arc"
-sudo -u wwwrun cp $dir/* `pwd` || exit 1
+sudo -u wwwrun cp $dir/* $PWD || exit 1
 rm -rf $dir
+
+echo "Перезапуск $dname"
+while sudo -u wwwrun killall -q $dname; do
+  sleep 1;
+  tail $log
+done
+$PWD/${prj}.sh
+tail -f $log
--- .ci/start.sh
+++ .ci/start.sh
@@ -4,7 +4,7 @@
 script_path=$(readlink -e $0)
 script_dir=$(dirname $script_path)
 project_name=$(basename $script_dir)
-project_path=$script_dir/$project_name
+project_path="$script_dir/${project_name}d"
 
 if [ ! -f "$project_path" ]; then
   echo "Имя запускаемого бинарника должно совпадать с именем директории"
@@ -12,10 +12,10 @@ if [ ! -f "$project_path" ]; then
 fi
 
 if [ -z $1 ]; then
-  if [[ "script_dir" == "$PWD" ]]; then
+  if [[ "$script_dir" == "$PWD" ]]; then
     conf="${project_name}.conf"
   else
-    conf="${project_path}.conf"
+    conf="$script_dir/${project_name}.conf"
   fi
   
   if [ ! -f $conf ]; then
